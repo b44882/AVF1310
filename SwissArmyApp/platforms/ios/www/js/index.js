a379e6b30d6f0b5dbd1a2e1abefebad0a1a1541f
Brett Gear <bgear@fullsail.edu>
2013-10-24 23:58:57 -0400

    Projecr 4 - #2 - Release Candidate

--- SwissArmyApp/platforms/ios/www/js/index.js
+++ SwissArmyApp/platforms/ios/www/js/index.js
@@ -86,6 +86,7 @@ function onDeviceReady(){
     	console.log("Geolocation Pressed");
     	$("#result").empty();
 			var onSuccess = function(position){
+				console.log("Geolocation Success");
 				var lat = position.coords.latitude;
 				var lon = position.coords.longitude;
 			$('<h2>Geolocation</h2>' +
@@ -99,6 +100,7 @@ function onDeviceReady(){
 			  '<li>Timestamp: '         + new Date(position.timestamp) + '</li>').appendTo("#result");
 		};
 		var onError = function(error){
+			console.log("Geolocation Error");
 			$('<h2>Geolocation Error</h2>' +
 			  '<ul>' +
 			  '<li>Code: '   + error.code + '</li>' +
@@ -153,10 +155,12 @@ function onDeviceReady(){
 			'Close Alert'            // buttonName
 		);
     });
+    
     //WEATHER BUTTON//
     $("#weather").on("click", function(){
     	console.log("Weather Pressed");
     	var onSuccess = function(position){
+    		console.log("Weather Success");
 			$("#result").empty();
 			var lat = position.coords.latitude;
 			var lon = position.coords.longitude;
@@ -173,6 +177,7 @@ function onDeviceReady(){
 			});
 		}
 		var onError = function(error){
+			console.log("Weather Error");
 			$('<h2>Geolocation Error</h2>' +
 			  '<ul>' +
 			  '<li>Code: '   + error.code + '</li>' +
@@ -180,9 +185,32 @@ function onDeviceReady(){
 		};
 		navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true});
 	});
-    
-    
-    
+	
+	//RESEARCH BUTTON//
+    $("#research").on("click", function(){
+    	console.log("Research Pressed");
+		$("#result").empty();
+		$('<h2>Discussion 1 - Project Intent</h2>' +
+		  '<p>	Social media outlooks like Twitter and Facebook allow it so that you can use information you may already be logged in with to create/sign in to website accounts.  It is fast and convenient when it comes to working with this, allowing users not having to do a whole extra process to log into the services they enjoy.  While you could probably do this for payments as well (aka, logging in with your twitter information you provided to pay for a bill), it may not be the safest route to go for security purposes.<p>' +
+		  '<p>	JSON-P, unlike JSON, allows you to communicate with different domains to obtain information.  Great examples would include a phone application that communicates with instagram to filter out certain pictures. <p>' +
+		  '<p>	I feel that any data resource that would be excellent for sharing photographs or streaming video services.  For example, an app that allows you to view the livestream of a gamer who has been playing a marathon of a popular game... while the application is grabbing the information via JSON-P to stream videos, there could be lists of pre-existing saved videos, as well as information of their game collection taken from instagram: which also uses JSON-P. </p>' +
+		  '<p>	Right now there is a toss up for two applications I would love to work on.  The first would be my education app, which would be a set of Nature Tools called "Swiss Army App.".  This promotes user to get out and explore the world,  with accesss to your camera which would allow you to upload to an instagram/facebook account, as well as access to compass/gps to help navigate through areas.  A second app I would love to develop personally is for any youtube personality looking to expand their audience through a free application.  I love the idea of creating an app that would communicate through youtube and display videos in the application itself.</p>' +
+		  '<p>	Simply put:  You need to spend a few hours a day for research, then design and development focuses.  This not only breaks up your work flow, but makes it so that you do not burn out too early during the week.  At the same time, this also prevents you from creating a rushed application the day before.</p>' +
+		  '<h2>Discussion 2 - Accessibility</h2>' +
+		  '<p>	Right now, the app is simple enough with HTML that not much needs to be changed around.  Of course, as the application becomes more complex, I would want to make sure that the code continues to be easy to read, as well as remains semantically sound.</p>'+
+		  '<p>	While not with the various applications themselves, which could be rather expensive, one thing that was mentioned in a video for screen reading is that normal website browsers cannot be used because of the various media that are on the webpages, it makes these tools very hard to use.  One thing to keep in mind with accessibility is perhaps a uniform way (perhaps semantically) to have text not displayed on the page, but still can be read by screen reads to help those visually impaired.</p>'+
+		  '<p>	Apple Braile Display: Contains Voiceover, and used with OS X.  Blackberry, with voice control for those that are visually impaired.</p>'+
+		  '<p>	Accessibility apps:  Voiceover, which allows the user to navigate between, and interact with, accessible elements in an application. BIG Launcher app which is an android application that displays various android interfaces larger for those that are visually impaired.</p>'+
+		  '<h2>Discussion 3 - Industry Research</h2>'+
+		  '<p>	If we were to look at the overall top application for all three countries, it would have to be game applications.  As we dig deeper though (top 10), it becomes more broad.</p>' +
+		  '<p>	While games still dominate U.S market, the top 10 is sprinkled with utility applications, such as weight loss and image editors. It also is good to mention that the games played are more casual based: they are designed with quick play in mind, and very little time vested.</p>'+
+          '<p>	Japan has the same data of games dominance in the top 10, with the same misc apps for music, videos, and weight loss sprinkled in.  The data is different here for Japan games however, as these game require more time vested, including various RPGs as well as former console games such as GTA Vice City. This shows that Japan customers perhaps spend more time with games on their phones then the U.S. Japan also has an interesting demographic in android markets for slot games in the top 2.</p>'+
+		  '<p>	In Great Britain shows almost similiar data as Japan and U.S, with games having a heavy dominance.  Great Britain also shows games with more time vested instead of casual games that can be played quickly for their most paid applications.</p>'+
+		  '<p>	Above all else though, when we talk about overall highest gross applications, free games with cash shops are dominant in all three markets.</p>'+
+		  '<p>In 2008, the average cost per mb was $0.46.  In 2012, the price has dropped dramatically to $0.03.  During these 4 years, an average user would use 149 MB in 2008, while in 2012, that number has skyrocked to over 1GB of data.  It is believe, with the way market is trending, the cost per MB will go down, while data usage skyrockets (up to 3,300+ MB in 2015).  Data caps right now are very high, with USA averaging anywhere from 150gb - 250gb a month, however, with mobile spots on the rise, it is advised to not consider this a big fear.  The point to take here is that as costs continue to drop, and with technology improvement, data usage will rise.  For now, we must mind the limits, and recommend that our application not exceed 50-100mb.</p>'+
+		  '<p>Games are a hot item right now, and in the mobile market it is no exception.  Applications which require frequent use are going to be the best path to go.  Games fulfull that requirement, and I believe in a U.S market, releasing something casual and free, with an optional in game market, will generate grossing money overtime.  My suggestion for this would be a simple puzzle game that can be completed anywhere between 2-5 minutes.  On an international scale, however, puzzle games I feel will not suffice.  The international market craves more in there games, and are willing to pay money to do so.  With that being said, something with more depth, such as RPGs are Action/Adventure games with a price tag will work.  I would also recommend a cash shop for these games, but without a big advertising push, as our U.S Markets. </p>'
+		  ).appendTo("#result")
+		});
 };
 
 document.addEventListener("deviceready", onDeviceReady, false);
